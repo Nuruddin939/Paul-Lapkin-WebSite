665c15b7979352617f848d941d8776d636bf2312
nuruddin939 <nuruddin3455@gmail.com>
2020-10-26 19:02:37 +0600

    first commit

---
+++ resources/js/custom.js
@@ -0,0 +1,24 @@
+// MixitUp
+
+var mixer = mixitup('.grid-portfolio-container');
+
+//Hamburger Menu
+
+function openMenu(){
+    document.getElementById('navbar').style.height = "100%";
+}
+
+function closeMenu(){
+    document.getElementById('navbar').style.height = "0%";
+}
+
+// Smooth Scroll
+
+$(function(){
+    $('.menu-item a, .next-section a, .totop a').on('click', function(){
+        $('html, body').animate({
+            scrollTop: $($.attr(this, 'href')).offset().top
+        }, 1000)
+    })
+})
+
